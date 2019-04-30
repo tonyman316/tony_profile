a6bdea70c7177c882b6a8fcce56d3eb483937e5b
Tony Man <tonyman316@gmail.com>
2019-04-29 22:40:06 -0700

    fix bugs

--- config/application.rb
+++ config/application.rb
@@ -6,15 +6,14 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.action_dispatch.default_headers = {
-  'X-Frame-Options' => 'ALLOWALL'
-}
 
 module TonyProfile
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    # config.action_dispatch.default_headers = {
+    #   'X-Frame-Options' => 'ALLOWALL'
+    # }
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
